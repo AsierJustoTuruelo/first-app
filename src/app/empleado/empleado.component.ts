9b5b663663a962ebf96bc00b4a7fc5b03939f454
AsierJustoTuruelo <100451012@alumnos.uc3m.es>
2023-10-19 20:24:46 +0200

    fourth commit

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+/* APUNTES
+
+Para crear property binding se usa [] --> [checked]="registrado"
+Para crear event binding se usa () --> (click)="setUsuarioRegistrado($event)
+Para crear binding bidireccional se usa [()] -->
+
+*/
 
 @Component({
   selector: 'app-empleado',
@@ -9,9 +17,39 @@ export class EmpleadoComponent {
   nombre = "Juan";
   apellido = "Justo";
   //edad = 20;
-  //empresa="Telefonica";
+  empresa="Google";
   
-  comprobarEdad(edad: number): "Es menor de edad" | "Es mayor de edad" | "No se puede determinar su edad" {
+  registrado = false;
+
+  textoRegistro ="No esta registrado, por favor registrese pulsando abajo.";
+
+  getRegistroUsuario(){
+    this.registrado=false;
+  }
+
+  setUsuarioRegistrado(evento:Event){
+    //this.textoRegistro = "Acaba de registrase, bienvenido a MOVISTAR.";
+    //alert ("El usuario se ha registrado");
+
+    if ((<HTMLInputElement>evento.target).value == "Si"){
+      this.textoRegistro = "Acaba de registrase, bienvenido a Empresa.";
+
+    } else{
+      this.textoRegistro = "No esta registrado, por favor registrese pulsando abajo.";
+
+    }
+  }
+
+  
+  
+
+  
+  
+  
+  
+  
+  
+  comprobarEdad(edad: number){
     if (edad < 18 && edad > 0) {
       return "Es menor de edad";
     } else if (edad >= 18) {
@@ -19,6 +57,10 @@ export class EmpleadoComponent {
     } else {
       return "No se puede determinar su edad";
     }
+  }
+
+  ngOnInit() {
+    // Código a ejecutar cuando se inicializa el componente
   }
   
 
