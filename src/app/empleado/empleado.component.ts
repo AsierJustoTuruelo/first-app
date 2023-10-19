65dcb9c0b57874f5ac44097f48e6383b9eccc975
AsierJustoTuruelo <100451012@alumnos.uc3m.es>
2023-10-19 17:55:26 +0200

    third commit; funciton and interpolation practica

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent {
+  nombre = "Juan";
+  apellido = "Justo";
+  //edad = 20;
+  //empresa="Telefonica";
+  
+  comprobarEdad(edad: number): "Es menor de edad" | "Es mayor de edad" | "No se puede determinar su edad" {
+    if (edad < 18 && edad > 0) {
+      return "Es menor de edad";
+    } else if (edad >= 18) {
+      return "Es mayor de edad";
+    } else {
+      return "No se puede determinar su edad";
+    }
+  }
+  
 
+
+  
 }
